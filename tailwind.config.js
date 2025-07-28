637e67a83accf913f58e1a7eef655316e4264d8c
Pahladine <adamcaza76@gmail.com>
2025-07-28 12:11:52 -0400

    fix: force Tailwind global CSS and path indexing

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}"
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./*.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {},
